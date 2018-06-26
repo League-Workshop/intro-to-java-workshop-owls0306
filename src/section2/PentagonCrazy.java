f251d8bacfc910b781b6ce49a7f20a696e0cfb70
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-26 14:56:41 -0700

    DAY 2

--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -24,7 +24,7 @@ public class PentagonCrazy {
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
 			int angle = 360/sides ;
 		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
-for 
+
 			// 2. Move the robot 200 pixels
 			djkitten.move(200);
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
